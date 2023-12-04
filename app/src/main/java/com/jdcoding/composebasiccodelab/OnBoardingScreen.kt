8ae8367a9850e45a494350de47303644a8b6a59d
JDangar <dangarjignesh9@gmail.com>
2023-12-04 23:03:16 +0530

    First commit

---
+++ app/src/main/java/com/jdcoding/composebasiccodelab/OnBoardingScreen.kt
@@ -0,0 +1,43 @@
+package com.jdcoding.composebasiccodelab
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jdcoding.composebasiccodelab.ui.theme.ComposeBasicCodelabTheme
+
+@Composable
+fun OnBoardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, heightDp = 320, widthDp = 320)
+@Composable
+fun OnBoardingPreview() {
+    ComposeBasicCodelabTheme {
+        OnBoardingScreen(onContinueClicked = {})
+    }
+}
